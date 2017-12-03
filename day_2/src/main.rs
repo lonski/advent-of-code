1f84d005a64ae6dc2fddec556726d9dcfbb2819b
lonski <michal@lonski.pl>
2017-12-03 11:56:58 +0100

    Day 2: part two

--- day_2/src/main.rs
+++ day_2/src/main.rs
@@ -6,7 +6,42 @@ fn main() {
     let mut input = String::new();
     file.read_to_string(&mut input).unwrap();
 
-    let checksum: u32 = input
+    println!("Part one: {}", part_one(&input));
+    println!("Part two: {}", part_two(&input));
+}
+
+fn part_two(input: &String) -> u32 {
+    input
+        .lines()
+        .map(|l| {
+            l.split("\t")
+                .map(|v| v.parse::<u32>().unwrap())
+                .collect::<Vec<u32>>()
+        })
+        .map(|values| divisable_tuple(&values))
+        .map(|t| t.0 / t.1)
+        .sum()
+    //.for_each(|v| println!("{:?}", v));
+}
+
+fn divisable_tuple(values: &Vec<u32>) -> (u32, u32) {
+    for v in values {
+        for v2 in values {
+            if v != v2 {
+                if v2 % v == 0 {
+                    return (*v2, *v);
+                }
+                if v % v2 == 0 {
+                    return (*v, *v2);
+                }
+            }
+        }
+    }
+    (0, 0)
+}
+
+fn part_one(input: &String) -> u32 {
+    input
         .lines()
         .map(|line| {
             (
@@ -21,7 +56,29 @@ fn main() {
             )
         })
         .map(|t| t.1 - t.0)
-        .sum();
+        .sum()
+}
 
-    println!("Checksum: {}", checksum);
+
+#[cfg(test)]
+mod tests {
+
+
+    use super::*;
+
+    #[test]
+    fn part_one_test() {
+        assert_eq!(
+            part_one(&String::from("5\t1\t9\t5\n7\t5\t3\n2\t4\t6\t8")),
+            18
+        );
+    }
+
+    #[test]
+    fn part_two_test() {
+        assert_eq!(
+            part_two(&String::from("5\t9\t2\t8\n9\t4\t7\t3\n3\t8\t6\t5")),
+            9
+        );
+    }
 }
