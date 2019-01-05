3593fa966cd10ca0dc664b9a83b9b7a2ef4a67ae
Michal Lonski <michal.lonski@nokia.com>
2019-01-05 20:09:55 +0100

    2018.Day_02

---
+++ 2018/day_02/inventory_management_system.rb
@@ -0,0 +1,24 @@
+ids = File.readlines 'input.1'
+
+checksum = ids.map do |id| 
+  counts = id.chars
+    .reduce(Hash.new(0)) { |acc, v| acc[v] += 1; acc }
+    .select{ |c, count| (2..3) === count }
+    .reduce([0,0]) { |acc, (k, v)| acc[v-2] = 1 if v > 0; acc }
+end
+  .reduce([0,0]) { |acc, v| acc[0] += v[0]; acc[1] += v[1]; acc }
+  .reduce(1, :*)
+
+puts "Checksum of box id list: #{checksum}"
+
+common_chars = ids.map(&:chars).map do |id|
+  ids.map(&:chars).map do |cmp|
+    tmp = id.clone
+    cmp.map{|c| c == tmp.shift ? c : '^'}
+  end
+end
+  .flat_map{ |v| v }
+  .select{ |v| v.count('^') == 1 }
+  .map{ |v| v.delete('^'); v}[0].join
+
+puts "Common chars are: #{common_chars}"
