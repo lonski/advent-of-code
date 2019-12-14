27971187c80f8d7dba5407fb267ae771da9d788d
Michał Łoński <michal@lonski.pl>
2019-12-14 12:38:08 +0100

    working on 10b@2019

--- 2019/day_10/solution.rb
+++ 2019/day_10/solution.rb
@@ -1,5 +1,5 @@
 def float_equal?(f1,f2)
-  (f2 - f1).abs <= 0.0001
+  (f2 - f1).abs <= 0.1
 end
 
 def distance(p1,p2)
@@ -7,6 +7,7 @@ def distance(p1,p2)
 end
 
 def on_line(a, b, start, goal)
+  #puts "#{start} #{goal}"
   if a == Float::INFINITY || a == -Float::INFINITY || b == Float::INFINITY || b == -Float::INFINITY || a.nan? || b.nan?
     dy = goal[1] - start[1]
     dx = goal[0] - start[0]
@@ -17,71 +18,116 @@ def on_line(a, b, start, goal)
   float_equal?(goal[1].to_f,  (a*goal[0].to_f + b))
 end
 
-map = File.read('input_3').split("\n")
+map = File.read('input_0').split("\n")
 width = map[0].size
 height = map.size
 
 map_points = (0..width-1).map {|x| (0..height-1).map{|y| [x,y] }}.flatten(1)
 asteroids = map_points.select{|x,y| map[y][x] != '.' }
 
-max_reachable = 0
-max_p = [0,0]
+def translate(base, point)
+  [point[0] - base[0], point[1] - base[1]]
+end
 
+def part2
 
-asteroids.each do |pos|
-
-  #pos = [1,2]
-  reachable = []
-  unreachable = []
-  asteroids.each do |x,y|
-    #x =2
-    #y =1
-    #
-    #puts "JEST" if (x == 8 && y == 6)
+  map = File.read('input_0').split("\n")
+  width = map[0].size
+  height = map.size
   
-    dy = y - pos[1]
-    dx = x - pos[0]
-  
-    a = dy.to_f / dx.to_f
-    b = (pos[1]*x - y*pos[0]).to_f / (x - pos[0]).to_f
+  map_points = (0..width-1).map {|x| (0..height-1).map{|y| [x,y] }}.flatten(1)
+  asteroids = map_points.select{|x,y| map[y][x] != '.' }
 
-    #puts "DDD"
-    #puts on_line(a,b,pos, [8,6])
-  
+  pos = [3,4]
+
+  (0..360).each do |degrees|
+    radians = degrees * Math::PI / 180
+    a = Math.tan(radians)
+
     on_line_list = asteroids
       .reject{|ast_p| ast_p == pos}
-      .select{|ast_p| on_line(a,b,pos,ast_p)}
+      .select{|ast_p| on_line(a,0.0,pos,translate(pos, ast_p))}
       .map{|ast_p| [distance(pos,ast_p),ast_p]}
       .sort_by{|dist, ast_p| dist}
-      .group_by{|dist, ast_p| dist < 0}
-
-
-    list_left = on_line_list[true] || []
-    list_right = on_line_list[false] || []
-
-    #puts "LL #{list_left.inspect}"
-    #puts "LR #{list_right.inspect}"
-
-    unless list_left.empty?
-      reachable << list_left.pop[1]
-      unreachable += list_left.map{|dist, ast_p| ast_p}
+      #group_by{|dist, ast_p| dist < 0}
+    
+    unless on_line_list.empty?
+      puts "deg=#{degrees} p=#{on_line_list.map{|dist, ast_p| ast_p}}"
     end
 
-    unless list_right.empty?
-      reachable << list_right.shift[1]
-      unreachable += list_right.map{|dist, ast_p| ast_p}
-    end
+    o1 = on_line(a, 0.0, pos, translate(pos, [1,0]))
+    o2 = on_line(a, 0.0, pos, translate(pos, [2,2]))
 
-    #break 
+    if o1 && o2
+      puts "d=#{degrees} o1=#{o1} o2=#{o2}"  
+    end
+  end
+end
+
+part2
+
+def part1
+  max_reachable = 0
+  max_p = [0,0]
+
+  asteroids.each do |pos|
+  
+    #pos = [1,2]
+    reachable = []
+    unreachable = []
+    asteroids.each do |x,y|
+      #x =2
+      #y =1
+    
+      dy = y - pos[1]
+      dx = x - pos[0]
+    
+      a = dy.to_f / dx.to_f
+      b = (pos[1]*x - y*pos[0]).to_f / (x - pos[0]).to_f
+  
+      #puts "DDD"
+      #puts on_line(a,b,pos, [8,6])
+    
+      on_line_list = asteroids
+        .reject{|ast_p| ast_p == pos}
+        .select{|ast_p| on_line(a,b,pos,ast_p)}
+        .map{|ast_p| [distance(pos,ast_p),ast_p]}
+        .sort_by{|dist, ast_p| dist}
+        .group_by{|dist, ast_p| dist < 0}
+  
+  
+      list_left = on_line_list[true] || []
+      list_right = on_line_list[false] || []
+  
+      #puts "LL #{list_left.inspect}"
+      #puts "LR #{list_right.inspect}"
+  
+      unless list_left.empty?
+        reachable << list_left.pop[1]
+        unreachable += list_left.map{|dist, ast_p| ast_p}
+      end
+  
+      unless list_right.empty?
+        reachable << list_right.shift[1]
+        unreachable += list_right.map{|dist, ast_p| ast_p}
+      end
+  
+      #break 
+    end
+    
+    reachable.uniq!
+    unreachable.uniq!
+    #puts reachable.size
+    if reachable.size > max_reachable
+      max_reachable = reachable.size
+      max_p = pos.dup
+    end
+  
   end
   
-  reachable.uniq!
-  unreachable.uniq!
-  #puts reachable.size
-  if reachable.size > max_reachable
-    max_reachable = reachable.size
-    max_p = pos.dup
-  end
+  puts
+  puts max_reachable
+  puts max_p.inspect
 
 end
 
@@ -92,8 +138,3 @@ end
 #puts "U: #{unreachable.inspect} -> #{unreachable.size}"
 #puts
 #puts "D: #{asteroids - reachable - unreachable}"
-puts
-puts max_reachable
-puts max_p.inspect
-
-
