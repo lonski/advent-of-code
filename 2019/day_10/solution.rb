f85cf8d5a8110721e1cc961b014c9006599cdd0f
Michal Lonski <michal@lonski.pl>
2019-12-14 17:51:10 +0100

    10b@2019 - failed attempt by dx increasing

--- 2019/day_10/solution.rb
+++ 2019/day_10/solution.rb
@@ -17,13 +17,22 @@ def distance(p1,p2)
   (p2[0] - p1[0]) + (p2[1] - p1[1])
 end
 
+def is_linear_fun?(a,b)
+  a != Float::INFINITY && 
+  a != -Float::INFINITY && 
+  b != Float::INFINITY &&
+  b != -Float::INFINITY &&
+  !a.nan? &&
+  !b.nan?
+end
+
 def on_line(a, b, start, goal)
   trace("\t\tONLINE a=#{a} b=#{b} s=#{start} e=#{goal}")
   if a == Float::INFINITY || a == -Float::INFINITY || b == Float::INFINITY || b == -Float::INFINITY || a.nan? || b.nan?
     dy = goal[1] - start[1]
     dx = goal[0] - start[0]
-    return goal[0] == start[0] if dy == 0
-    return goal[1] == start[1]
+    trace("\t\tINF dy=#{dy} dx=#{dx}")
+    return dx == 0 || dy == 0
   end
   trace("\t\tLINE a=#{a} b=#{b} Y=#{a*goal[0].to_f + b} goal[1]=#{goal[1]} ")
   float_equal?(goal[1].to_f,  (a*goal[0].to_f + b))
@@ -42,7 +51,7 @@ end
 
 def part2
 
-  map = File.read('input_0').split("\n")
+  map = File.read('input_3').split("\n")
   width = map[0].size
   height = map.size
   
@@ -50,7 +59,7 @@ def part2
   asteroids = map_points.select{|x,y| map[y][x] != '.' }
   counter = 0
 
-  pos = [3,4]
+  pos = [8,3]
 
   #get objects in quarter I
   q1_objects = asteroids
@@ -63,30 +72,47 @@ def part2
     #find objects on given dx, sorted by y desc
     obj_on_dx = q1_objects
       .select{|ox, oy| ox - pos[0] == dx}
-      .sort_by{|ox, oy| -oy}
+      .sort_by{|ox, oy| oy}
 
     debug("dx=#{dx} objs=#{obj_on_dx.inspect}")
+
+    #list of objects to skip due to be blocked
+    to_skip = []
 
     #for each object of above find all objects on the line
     obj_on_dx.each do |x, y|
       debug("  checking [#{x},#{y}]")
       debug("  q1_objects=#{q1_objects.inspect}")
+      debug("  to_skip=#{to_skip.inspect}")
+      next if to_skip.include?([x,y])
       # calculate linear function factors
       dy = y - pos[1]
       dx = x - pos[0]
       a = dy.to_f / dx.to_f
       b = (pos[1]*x - y*pos[0]).to_f / (x - pos[0]).to_f
-      #find all other objects that are on the same line, sorted by distance asc
-      on_line_objs = q1_objects
-        .select{|o| r = on_line(a,b,[x,y],o); trace("\t\tONLINE RET = #{r}"); r}
-        .sort_by{|o| distance([x,y], o)}
+
+      #find all other objects that are on the same line
+      on_line_objs = []
+      if is_linear_fun?(a,b)
+        on_line_objs = q1_objects.select{|o| r = on_line(a,b,[x,y],o); trace("\t\tONLINE RET = #{r}"); r}
+      else
+        #handle non linear fun, only for Q1
+        on_line_objs = q1_objects.select{|ox, oy| ox == pos[0]}
+      end
+
+      #sort on_line objects by distance
+      on_line_objs = on_line_objs.sort_by{|o| distance([x,y], o)}
       debug("   #{[x,y].inspect} line: #{on_line_objs.inspect}")
+
       #get closest object and vaporize
       unless on_line_objs.empty?
         to_vaporize = on_line_objs.first
-        counter += 1
-        asteroids = asteroids - [to_vaporize]
-        puts "Vaporized #{to_vaporize.inspect} counter=#{counter}"
+        if asteroids.include? to_vaporize
+          counter += 1
+          asteroids = asteroids - [to_vaporize]
+          to_skip += (on_line_objs - [to_vaporize])
+          puts "Vaporized #{to_vaporize.inspect} counter=#{counter}"
+        end
       end
     end
 
